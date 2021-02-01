1e66645622fc75e77035af478fd5bdb237580e36
Anirvin Vaddiyar <anirvinv@gmail.com>
2021-02-01 16:08:01 -0500

    ----

--- source/NeuralNet.py
+++ source/NeuralNet.py
@@ -40,15 +40,39 @@ class Layer(object):
 class Network(object):
     def __init__(self, features, n_layers, neurons_per_layer):
         self.layers = []
+        self.outputs = []
+    #Initializes all the dimensions
         self.layers.append(Layer(neurons_per_layer[0], features[0]))
         for i in range(1, n_layers):
             self.layers.append(Layer(neurons_per_layer[i], self.layers[i-1].output()))
+        if not len(self.layers[-1].neurons) == 1:
+            self.layers.append(Layer(1, self.layers[-1].output()))
+        
+    def predicted_values(self):
+        self.outputs.append(self.layers[-1].output())
+
+        for index, feature in enumerate(features):
+            if index == 0:
+                continue
+            else:
+                self.layers[0].input = feature
+                print(self.layers[0].output())
+                for i in range(1, len(self.layers)):
+                    self.layers[i].input = self.layers[i-1].output()
+                out = self.layers[-1].output()
+                self.outputs.append(out)
+        return self.outputs
+
+    
+
+
     def __repr__(self): 
         return f"{['-----'+layer.__repr__() + '-----' for layer in self.layers]}"
         
 
-features = [[1,2,3,4]]
+features = [[1,2,3,4], [5,6,7,8]]
 
 net = Network(features, 3, (4,2,3,4))
 
 print(net)
+# print(net.predicted_values())
